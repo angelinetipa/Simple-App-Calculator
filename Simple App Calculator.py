a01812e7b995e395c40222e07a5cbf52fde12ba4
angelinetipa <angelinetipa@gmail.com>
2023-05-07 17:29:51 +0800

    pseudocode

---
+++ Simple App Calculator.py
@@ -0,0 +1,12 @@
+# use try catch
+# input for math operation
+# input for two numbers
+# if math operation is addtion, then add the two numbers
+# if math operation is subtraction, then subtract the two numbers
+# if math operation is multiplication, then multiply the two numbers
+# if math operation is division, then divide the two numners
+# output of the result
+# input for try again
+# while try again is not no, repeat Step 1
+# else, display "Thank you"
+# exit program
