9d597c877ee23c86e55712b99d1768232437e12e
angelinetipa <angelinetipa@gmail.com>
2023-05-07 17:37:13 +0800

    used exception

--- Simple App Calculator.py
+++ Simple App Calculator.py
@@ -1,12 +1,15 @@
 # use try catch
+try:
 # input for math operation
 # input for two numbers
 # if math operation is addtion, then add the two numbers
 # if math operation is subtraction, then subtract the two numbers
 # if math operation is multiplication, then multiply the two numbers
 # if math operation is division, then divide the two numners
+except:
 # output of the result
 # input for try again
 # while try again is not no, repeat Step 1
 # else, display "Thank you"
+finally:
 # exit program
