652e79273b227219f2798f1a1212412852d2b849
angelinetipa <angelinetipa@gmail.com>
2023-05-07 18:08:17 +0800

    input for math op

--- Simple App Calculator.py
+++ Simple App Calculator.py
@@ -1,15 +1,23 @@
 # use try catch
 try:
-# input for math operation
+    # input for math operation
+    math_operation = input("Addition Subtraction Multiplication Division \nWhat math operation would you like to use?  ").strip().lower()
 # input for two numbers
 # if math operation is addtion, then add the two numbers
 # if math operation is subtraction, then subtract the two numbers
 # if math operation is multiplication, then multiply the two numbers
 # if math operation is division, then divide the two numners
-except:
+except SyntaxError:
+    print("Something is wrong with syntax")
+except ZeroDivisionError:
+    print("Something is wrong with division")
+except ValueError:
+    print("Something is wrong with printing value")
+
 # output of the result
 # input for try again
 # while try again is not no, repeat Step 1
 # else, display "Thank you"
-finally:
 # exit program
+finally:
+    print("End of program.")
