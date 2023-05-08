5cc87f638a6e947aa7544e41dc2c653f21eff2f7
angelinetipa <angelinetipa@gmail.com>
2023-05-08 16:47:52 +0800

    did few changes

--- Simple App Calculator.py
+++ Simple App Calculator.py
@@ -33,13 +33,12 @@ def calculator():
         elif math_operation == "4":
             print(f"{Fore.CYAN}{first_number} / {second_number} = ", float(first_number) / float(second_number)) 
         # input for try again
-        try_again = input(f"{Style.RESET_ALL}{Style.BRIGHT}Do you want to try again?😁  {Style.RESET_ALL}").strip().lower()
+        try_again = input(f"{Style.RESET_ALL}{Style.BRIGHT}Do you want to try again (yes/no)?😁  {Style.RESET_ALL}").strip().lower()
         # clear terminal
         time.sleep(0.5)
         os.system("cls")
-                
-    # else, display "Thank you"
-    print(f"{Fore.BLACK}{Back.WHITE}{Style.BRIGHT}➕ ➖ ✖ ➗   S I M P L E     A P P     C A L C U L A T O R   ➗ ✖ ➖ ➕ {Style.RESET_ALL}\n{Style.BRIGHT}Thank you for using my Simple App Calculator! ♥")
+        # else, display "Thank you"
+        print(f"{Fore.BLACK}{Back.WHITE}{Style.BRIGHT}➕ ➖ ✖ ➗   S I M P L E     A P P     C A L C U L A T O R   ➗ ✖ ➖ ➕ {Style.RESET_ALL}\n{Style.BRIGHT}Thank you for using my Simple App Calculator! ♥")
     
 # use try catch
 try:
@@ -48,35 +47,47 @@ try:
 # exception for syntax error
 except SyntaxError:
     print(f"{Fore.RED}Something is wrong with syntax.")
-    time.sleep(3)
-    try_again = input(f"{Style.RESET_ALL}{Style.BRIGHT}Do you want to try again?😁  {Style.RESET_ALL}").strip().lower()
+    time.sleep(2.5)
+    try_again = input(f"{Style.RESET_ALL}{Style.BRIGHT}Do you want to try again (yes/no)?😁  {Style.RESET_ALL}").strip().lower()
     if try_again == "yes":
         #call calculator to repeat the process
         calculator()
+    # last message
+    os.system("cls")
+    print(f"{Fore.BLACK}{Back.WHITE}{Style.BRIGHT}➕ ➖ ✖ ➗   S I M P L E     A P P     C A L C U L A T O R   ➗ ✖ ➖ ➕ {Style.RESET_ALL}\n{Style.BRIGHT}Thank you for using my Simple App Calculator! ♥")
 # exception for zero division error
 except ZeroDivisionError:
-    print(f"{Fore.RED}Something is wrong with division. You are dividing by zero! Try Again.")
-    time.sleep(3)
-    try_again = input(f"{Style.RESET_ALL}{Style.BRIGHT}Do you want to try again?😁  {Style.RESET_ALL}").strip().lower()
+    print(f"{Fore.RED}Something is wrong with division. You are dividing by zero!")
+    time.sleep(2.5)
+    try_again = input(f"{Style.RESET_ALL}{Style.BRIGHT}Do you want to try again (yes/no)?😁  {Style.RESET_ALL}").strip().lower()
     if try_again == "yes":
         #call calculator to repeat the process
         calculator()
+    # last message
+    os.system("cls")
+    print(f"{Fore.BLACK}{Back.WHITE}{Style.BRIGHT}➕ ➖ ✖ ➗   S I M P L E     A P P     C A L C U L A T O R   ➗ ✖ ➖ ➕ {Style.RESET_ALL}\n{Style.BRIGHT}Thank you for using my Simple App Calculator! ♥")
 # exception for value error
 except ValueError:
-    print(f"{Fore.RED}Some value is invalid for a given operation. Try Again.")
-    time.sleep(3)
-    try_again = input(f"{Style.RESET_ALL}{Style.BRIGHT}Do you want to try again?😁  {Style.RESET_ALL}").strip().lower()
+    print(f"{Fore.RED}Some value is invalid for a given operation.")
+    time.sleep(2.5)
+    try_again = input(f"{Style.RESET_ALL}{Style.BRIGHT}Do you want to try again (yes/no)?😁  {Style.RESET_ALL}").strip().lower()
     if try_again == "yes":
         #call calculator to repeat the process
         calculator()
+    # last message
+    os.system("cls")
+    print(f"{Fore.BLACK}{Back.WHITE}{Style.BRIGHT}➕ ➖ ✖ ➗   S I M P L E     A P P     C A L C U L A T O R   ➗ ✖ ➖ ➕ {Style.RESET_ALL}\n{Style.BRIGHT}Thank you for using my Simple App Calculator! ♥")
 # exception for value error
 except:
-    print(f"{Fore.RED}Unkown Exception. Try Again.")
-    time.sleep(3)
-    try_again = input(f"{Style.RESET_ALL}{Style.BRIGHT}Do you want to try again?😁  {Style.RESET_ALL}").strip().lower()
+    print(f"{Fore.RED}Unkown Exception. ")
+    time.sleep(2.5)
+    try_again = input(f"{Style.RESET_ALL}{Style.BRIGHT}Do you want to try again (yes/no)?😁  {Style.RESET_ALL}").strip().lower()
     if try_again == "yes":
         #call calculator to repeat the process
         calculator()
+    # last message
+    os.system("cls")
+    print(f"{Fore.BLACK}{Back.WHITE}{Style.BRIGHT}➕ ➖ ✖ ➗   S I M P L E     A P P     C A L C U L A T O R   ➗ ✖ ➖ ➕ {Style.RESET_ALL}\n{Style.BRIGHT}Thank you for using my Simple App Calculator! ♥")
 # exit program
 finally:
     print(f"{Style.BRIGHT}Bye Bye!")
